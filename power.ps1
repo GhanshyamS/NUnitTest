ca33a4cbae6ca6275fc7ea6aca3416eaeacf61cb
Kevinpiper <subedi.ghanshyam@gmail.com>
2021-09-30 21:51:18 +0545

    vardel

--- power.ps1
+++ power.ps1
@@ -1,4 +1,4 @@
 Write-Host "Congratulations! Your first script executed successfully"
-Write-Host "WORKSPAC== $WORKSPACE ==== `n $env:customWorkspace $customWorkspace"
+Write-Host "WORKSPAC== $WORKSPACE ==== `n"
 #New-Item -Path "..\" -Name "logfiles" -ItemType "directory"
 
