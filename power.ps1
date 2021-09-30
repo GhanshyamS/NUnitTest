61ccd61a93be53a13c51ddc493034f4aa852c23e
Kevinpiper <subedi.ghanshyam@gmail.com>
2021-09-30 16:12:37 +0545

    vardel

--- power.ps1
+++ power.ps1
@@ -1,4 +1,4 @@
 Write-Host "Congratulations! Your first script executed successfully"
 Write-Host "WORKSPACE var == $env:WORKSPACE ===="
-#New-ItemType Directory -Force -Path .\Teset
+#New-Item Type Directory -Force -Path .\Teset
 
