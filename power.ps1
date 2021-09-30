63f6b1e12ca6a341d94549f4f7782c4206e11ff5
Kevinpiper <subedi.ghanshyam@gmail.com>
2021-09-30 22:11:54 +0545

    vardel

--- power.ps1
+++ power.ps1
@@ -1,7 +1,9 @@
 Write-Host "Congratulations! Your first script executed successfully"
 Write-Host "WORKSPAC== $env:WORKSPACE  NUnitConsoleApp/NUnitConsoleApp.sln==== `n"
-Set-Location -Path "$env:WORKSPACE/NUnitConsoleApp/NUnitConsoleApp.sln"
-Write-Host "current loc = "Get-Location 
+Set-Location -Path $env:WORKSPACE"\NUnitConsoleApp"
+Write-Host "current loc = " Get-Location 
+Set-Location -Path "NUnitConsoleApp"
+Write-Host "current loc = " Get-Location 
 
 #New-Item -Path "..\" -Name "logfiles" -ItemType "directory"
 
