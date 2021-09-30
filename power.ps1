b4bb988c130220ef4672da23d2bdac33af97bac4
Kevinpiper <subedi.ghanshyam@gmail.com>
2021-09-30 20:27:08 +0545

    vardel

--- power.ps1
+++ power.ps1
@@ -1,4 +1,4 @@
 Write-Host "Congratulations! Your first script executed successfully"
-Write-Host "WORKSPAC== $env:WORKSPACE ==== Job name = $env:JOB_NAME"
-New-Item -Path "..\" -Name "logfiles" -ItemType "directory"
+Write-Host "WORKSPAC== $env:WORKSPACE ==== `n Job name = $env:JOB_NAME `n node = $env:NODE_NAME `n nodelevel = $env:NODE_LABELS"
+#New-Item -Path "..\" -Name "logfiles" -ItemType "directory"
 
